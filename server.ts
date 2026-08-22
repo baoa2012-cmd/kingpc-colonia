7ee5e13797bcedec79c7544c4a906d1aa58d3940
King PC Colonia <kingpccolonia@gmail.com>
2026-08-21 21:01:54 -0300

    Support dynamic PORT configuration

--- server.ts
+++ server.ts
@@ -516,7 +516,7 @@ function extraerDatosDirectos(texto: string) {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3002;
 
   app.use(express.json({ limit: "20mb" }));
 
